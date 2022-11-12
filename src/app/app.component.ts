bc542487f3a37e1ec2bfbe3d4fc8c4917fd018d8
jianyilai <2002854D@student.tp.edu.sg>
2022-11-12 14:15:58 +0800

    Added code for Chatbot integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coaching-chatbot';
+  ngOnInit(){
+    (function(d, m){
+      var kommunicateSettings = 
+          {"appId":"1606ebc34acf0257478a1137196af0efa","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      (window as any).kommunicate = m; m._globals = kommunicateSettings;
+  })(document, (window as any).kommunicate || {});
+  };
 }
