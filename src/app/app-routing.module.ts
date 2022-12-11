e40dfabca72150304f1ceb8666b64a41663a36c7
jianyilai <2002854D@student.tp.edu.sg>
2022-12-12 01:15:21 +0800

    Removed settings and reminder components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SettingsComponent } from './settings/settings.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ToDoComponent } from './to-do/to-do.component';
-import { RemindersComponent } from './reminders/reminders.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth.guard';
@@ -11,9 +9,7 @@ import { AuthGuard } from './auth.guard';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard], data: { permission: { only: ["user"] } } },
-  { path: 'reminders', component: RemindersComponent },
   { path: 'to-do', component: ToDoComponent },
-  { path: 'settings', component: SettingsComponent },
   { path: '', component: HomeComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { IonicModule } from "@ionic/angular";
-import { SettingsComponent } from './settings/settings.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ToDoComponent } from './to-do/to-do.component';
-import { RemindersComponent } from './reminders/reminders.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TokenInterceptor } from "./token-interceptor.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SettingsComponent,
     NavBarComponent,
     LoginComponent,
     ProfileComponent,
     ToDoComponent,
-    RemindersComponent,
     HomeComponent
   ],
   imports: [
